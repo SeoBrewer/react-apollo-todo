d7926043c4dbc041ce9b17355f43d97acf37fa5a
Armando Nougues <armando.nougues@sectoralarm.eu>
2020-04-29 16:55:24 +0200

    first commit

---
+++ src/components/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'army-testing.eu.auth0.com',
+  clientId: 'J7uMNcZbr8CtXxUJqz42Aqqa8jLn4mfG',
+  callbackUrl: 'http://localhost:3000/callback',
+  afterLogout: 'http://localhost:3000'
+};
